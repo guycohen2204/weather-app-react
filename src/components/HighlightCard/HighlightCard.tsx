3a6e7f876e1302cbc3bcd41763dd5a6176272dac
guycohen2204 <guyco@wix.com>
2024-12-01 19:34:44 +0200

    css-fices

--- src/components/HighlightCard/HighlightCard.tsx
+++ src/components/HighlightCard/HighlightCard.tsx
@@ -4,18 +4,20 @@ import styles from './HighlightCard.module.css';
 import { roundString } from '../../utils/roundString';
 
 type Props = {
-    title: string;
-    value?: string;
-    symbol?: string;
-}
+	title: string;
+	value?: string;
+	symbol?: string;
+};
 
 const HighlightCard = ({ title, value, symbol }: Props) => {
-    return (
-        <div className={styles.container}>
-            <h5 className={styles.title}>{title}</h5>
-            <p>{value ? roundString(value) : 'N/A'} {symbol}</p>
-        </div>
-    );
+	return (
+		<div className={styles.container}>
+			<h5 className={styles.title}>{title}</h5>
+			<p>
+				{roundString(value)} {symbol}
+			</p>
+		</div>
+	);
 };
 
 export default HighlightCard;
--- src/utils/roundString.ts
+++ src/utils/roundString.ts
@@ -1,3 +1,5 @@
-export const roundString = (s: string): string => {
-	return Math.round(+s).toString();
+export const roundString = (s?: string): string => {
+	if (s)
+		return Math.round(+s).toString();
+	return '0'
 }
